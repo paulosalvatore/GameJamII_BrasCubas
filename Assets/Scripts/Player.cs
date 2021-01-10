ec2196d359ccba1f35e420469c6f0dcfe7b87963
Paulo Salvatore <salvatoregames@gmail.com>
2021-01-10 01:34:24 -0300

    Basic player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,4 +2,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
+
+    [SerializeField]
+    private Transform armPivot;
+
+    [SerializeField]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private float moveSpeed;
+
+    private void Update()
+    {
+        rb.velocity = new Vector3(
+            -moveSpeed,
+            rb.velocity.y,
+            rb.velocity.z
+        );
+
+        armPivot.Rotate(Vector3.forward * (rotateSpeed * Time.deltaTime));
+    }
 }
